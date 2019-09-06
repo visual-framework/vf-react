ae6df76eac85b0447397a66beea7a72dee453941
Ken Hawkins <khawkins98@gmail.com>
2019-09-06 13:57:46 +0100

    Build vf components to src/vf-generated-assets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import '../css/styles.css';
+import './vf-generated-assets/css/styles.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
