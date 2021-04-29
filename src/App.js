2b2b9e6ba9c6ce4e32b77e4480678e848e1d3a96
Nitin Jadhav <nitinjadhav@gmail.com>
2021-04-29 15:22:23 +0530

    Added documentation playground page and react button example (#50)
    * Added documentation playground page and  react button example
    
    * Add a cheeky commit to update components add vf-navigation
    
    * className typos
    
    Co-authored-by: Nitin Jadhav <nitin@ebi.ac.uk>
    Co-authored-by: Ken Hawkins <khawkins98@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Route, Link, NavLink } from "react-router-dom";
 import VfLogoPath from '@visual-framework/vf-logo/assets/logo.svg';
 import Home from './Home';
 import TestPage from './TestPage';
+import ComponentShowcase from './components/components-showcase/ComponentShowcase';
 // import './vf-components/vf-componenet-rollup/index.scss';
 
 // VF njk templates
@@ -11,28 +12,43 @@ import TestPage from './TestPage';
 function App() {
   return (
     <Router basename={'/vf-react'}>
-      <div className="vf-body">
+      <div className="vf-body | vf-stack vf-stack--400">
         <header className="vf-global-header">
           <NavLink className="vf-logo" to="/">
-            <img className="vf-logo__image" src={VfLogoPath} alt="Visual Framework 2.0" />	
-            <span className="vf-logo__text">Visual Framework 2.0</span>	
+            <img className="vf-logo__image" src={VfLogoPath} alt="Visual Framework 2.0" />
           </NavLink>
-          <nav class="vf-navigation vf-navigation--global | vf-cluster">
-            <ul class="vf-navigation__list | vf-list | vf-cluster__inner">
+
+          <nav className="vf-navigation vf-navigation--global | vf-cluster">
+            <ul className="vf-navigation__list | vf-list | vf-cluster__inner">
+              
               <li className="vf-navigation__item">
-                <a href="https://visual-framework.github.io/vf-welcome/" className="vf-navigation__link">About the Visual Framework</a>
+                <a href="/search" className="vf-navigation__link">Search</a>
               </li>
               <li className="vf-navigation__item">
-                <a href="https://visual-framework.github.io/vf-welcome/documentation" className="vf-navigation__link">Documentation</a>
+                <a href="https://github.com/visual-framework/vf-core" className="vf-navigation__link">GitHub</a>
               </li>
               <li className="vf-navigation__item">
-                <a href="https://discord.gg/XHAvkUX" className="vf-navigation__link">Help, chat</a>
+                <a href="https://join.slack.com/t/visual-framework/shared_invite/enQtNDAxNzY0NDg4NTY0LWFhMjEwNGY3ZTk3NWYxNWVjOWQ1ZWE4YjViZmY1YjBkMDQxMTNlNjQ0N2ZiMTQ1ZTZiMGM4NjU5Y2E0MjM3ZGQ" className="vf-navigation__link">Slack</a>
               </li>
             </ul>
           </nav>
         </header>
+        <nav className="vf-navigation vf-navigation--main | vf-cluster">
+          <ul className="vf-navigation__list | vf-list | vf-cluster__inner">
+            <li className="vf-navigation__item">
+              <NavLink exact={true} to="/" className="vf-navigation__link">Home</NavLink>
+            </li>
+            <li className="vf-navigation__item">
+              <NavLink exact={true} to="/testpage" className="vf-navigation__link">Sample page</NavLink>
+            </li>
+            <li className="vf-navigation__item">
+              <NavLink to="/components-showcase" className="vf-navigation__link">Components Showcase</NavLink>
+            </li>
+          </ul>
+        </nav>
 
         <Route exact path="/" component={Home} />
+        <Route path="/components-showcase" component={ComponentShowcase} />
         <Route path="/testpage" component={TestPage} />
       </div>
     </Router>
