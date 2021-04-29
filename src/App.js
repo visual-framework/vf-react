2a1a7cda237fb1fdd13a2d3b4864f33c1569cada
Ken Hawkins <khawkins98@gmail.com>
2021-04-29 09:55:46 +0200

    className typos

--- src/App.js
+++ src/App.js
@@ -13,35 +13,35 @@ function App() {
   return (
     <Router basename={'/vf-react'}>
       <div className="vf-body | vf-stack vf-stack--400">
-        <header class="vf-global-header">
+        <header className="vf-global-header">
           <NavLink className="vf-logo" to="/">
             <img className="vf-logo__image" src={VfLogoPath} alt="Visual Framework 2.0" />
           </NavLink>
 
-          <nav class="vf-navigation vf-navigation--global | vf-cluster">
-            <ul class="vf-navigation__list | vf-list | vf-cluster__inner">
+          <nav className="vf-navigation vf-navigation--global | vf-cluster">
+            <ul className="vf-navigation__list | vf-list | vf-cluster__inner">
               
-              <li class="vf-navigation__item">
-                <a href="/search" class="vf-navigation__link">Search</a>
+              <li className="vf-navigation__item">
+                <a href="/search" className="vf-navigation__link">Search</a>
               </li>
-              <li class="vf-navigation__item">
-                <a href="https://github.com/visual-framework/vf-core" class="vf-navigation__link">GitHub</a>
+              <li className="vf-navigation__item">
+                <a href="https://github.com/visual-framework/vf-core" className="vf-navigation__link">GitHub</a>
               </li>
-              <li class="vf-navigation__item">
-                <a href="https://join.slack.com/t/visual-framework/shared_invite/enQtNDAxNzY0NDg4NTY0LWFhMjEwNGY3ZTk3NWYxNWVjOWQ1ZWE4YjViZmY1YjBkMDQxMTNlNjQ0N2ZiMTQ1ZTZiMGM4NjU5Y2E0MjM3ZGQ" class="vf-navigation__link">Slack</a>
+              <li className="vf-navigation__item">
+                <a href="https://join.slack.com/t/visual-framework/shared_invite/enQtNDAxNzY0NDg4NTY0LWFhMjEwNGY3ZTk3NWYxNWVjOWQ1ZWE4YjViZmY1YjBkMDQxMTNlNjQ0N2ZiMTQ1ZTZiMGM4NjU5Y2E0MjM3ZGQ" className="vf-navigation__link">Slack</a>
               </li>
             </ul>
           </nav>
         </header>
-        <nav class="vf-navigation vf-navigation--main | vf-cluster">
-          <ul class="vf-navigation__list | vf-list | vf-cluster__inner">
-            <li class="vf-navigation__item">
+        <nav className="vf-navigation vf-navigation--main | vf-cluster">
+          <ul className="vf-navigation__list | vf-list | vf-cluster__inner">
+            <li className="vf-navigation__item">
               <NavLink exact={true} to="/" className="vf-navigation__link">Home</NavLink>
             </li>
-            <li class="vf-navigation__item">
+            <li className="vf-navigation__item">
               <NavLink exact={true} to="/testpage" className="vf-navigation__link">Sample page</NavLink>
             </li>
-            <li class="vf-navigation__item">
+            <li className="vf-navigation__item">
               <NavLink to="/components-showcase" className="vf-navigation__link">Components Showcase</NavLink>
             </li>
           </ul>
